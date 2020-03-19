d238625641338331bba35b5d695f868c468e0c54
Karan <k123parmar@gmail.com>
2020-03-19 14:39:16 +0530

    Updates

---
+++ precache-manifest.28b15d79bf03c538bf38ff93af1e8a28.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd782a24597fc4bfedb510fb929b3982",
+    "url": "/contact-app/index.html"
+  },
+  {
+    "revision": "fc7ba6209079b130082b",
+    "url": "/contact-app/static/css/2.21dd1a39.chunk.css"
+  },
+  {
+    "revision": "5a2f4f1f9b62209bc75e",
+    "url": "/contact-app/static/css/main.a89fabe4.chunk.css"
+  },
+  {
+    "revision": "fc7ba6209079b130082b",
+    "url": "/contact-app/static/js/2.4a181733.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/contact-app/static/js/2.4a181733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2f4f1f9b62209bc75e",
+    "url": "/contact-app/static/js/main.68c8fe43.chunk.js"
+  },
+  {
+    "revision": "4834d1f3cfbe55ee5d18",
+    "url": "/contact-app/static/js/runtime-main.cb2acef6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contact-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contact-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contact-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contact-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contact-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
